e9a96c5f4b33c3cf0d57d7a40ab28bfdda134e99
DeathRustZzZ <Rustforthefuture@yandex.ru>
2026-06-23 16:08:45 +0300

    refactor(application): make repository save take immutable references

--- crates/application/src/customer/ports.rs
+++ crates/application/src/customer/ports.rs
@@ -3,7 +3,7 @@ use domain::CustomerId;
 use domain::customer::aggregate::Customer;
 
 pub trait CustomerRepository {
-    fn save(&self, customer: &mut Customer) -> Result<(), RepositoryError>;
+    fn save(&self, customer: &Customer) -> Result<(), RepositoryError>;
 
     fn find_by_id(&self, customer_id: CustomerId) -> Result<Option<Customer>, RepositoryError>;
 }
--- crates/application/src/vehicle/ports.rs
+++ crates/application/src/vehicle/ports.rs
@@ -3,7 +3,7 @@ use domain::VehicleId;
 use domain::vehicle::aggregate::Vehicle;
 
 pub trait VehicleRepository {
-    fn save(&self, vehicle: &mut Vehicle) -> Result<(), RepositoryError>;
+    fn save(&self, vehicle: &Vehicle) -> Result<(), RepositoryError>;
 
     fn find_by_id(&self, vehicle_id: VehicleId) -> Result<Option<Vehicle>, RepositoryError>;
 }
