9567c1f01a4a40cf88992e84e7083c72e05cef15
DeathCrushZzZ <Rustforthefuture@yandex.ru>
2026-06-23 15:15:35 +0300

    feat(application): add vehicle ownership repository trait

--- crates/application/src/ownership/ports.rs
+++ crates/application/src/ownership/ports.rs
@@ -1,4 +1,14 @@
-#[allow(unused_imports)]
 use crate::error::RepositoryError;
+use domain::vehicle_ownership::aggregate::VehicleOwnership;
+use domain::{VehicleId, VehicleOwnershipId};
 
-pub trait VehicleOwnershipRepository {}
+pub trait VehicleOwnershipRepository {
+    fn has_active_ownership(&self, vehicle_id: VehicleId) -> Result<bool, RepositoryError>;
+
+    fn save(&self, ownership: &VehicleOwnership) -> Result<(), RepositoryError>;
+
+    fn find_by_id(
+        &self,
+        ownership_id: VehicleOwnershipId,
+    ) -> Result<Option<VehicleOwnership>, RepositoryError>;
+}
