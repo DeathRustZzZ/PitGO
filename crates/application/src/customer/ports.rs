86d986e32487c5bb7a6860b7f6951139d3fdc9a7
DeathRustZzZ <Rustforthefuture@yandex.ru>
2026-06-23 12:24:09 +0300

    refactor(application): move vehicle ports out of customer module

--- crates/application/src/customer/ports.rs
+++ crates/application/src/customer/ports.rs
@@ -1,19 +1,9 @@
 use crate::error::RepositoryError;
 use domain::CustomerId;
-use domain::VehicleId;
 use domain::customer::aggregate::Customer;
-use domain::vehicle::aggregate::Vehicle;
 
 pub trait CustomerRepository {
     fn save(&self, customer: &mut Customer) -> Result<(), RepositoryError>;
 
     fn find_by_id(&self, customer_id: CustomerId) -> Result<Option<Customer>, RepositoryError>;
 }
-
-pub trait VehicleRepository {
-    fn save(&self, vehicle: &mut Vehicle) -> Result<(), RepositoryError>;
-
-    fn find_by_id(&self, vehicle_id: VehicleId) -> Result<Option<Vehicle>, RepositoryError>;
-}
-
-pub trait VehicleOwnershipRepository {}
---
+++ crates/application/src/vehicle/ports.rs
@@ -0,0 +1,9 @@
+use crate::error::RepositoryError;
+use domain::VehicleId;
+use domain::vehicle::aggregate::Vehicle;
+
+pub trait VehicleRepository {
+    fn save(&self, vehicle: &mut Vehicle) -> Result<(), RepositoryError>;
+
+    fn find_by_id(&self, vehicle_id: VehicleId) -> Result<Option<Vehicle>, RepositoryError>;
+}
