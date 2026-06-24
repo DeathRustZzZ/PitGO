3cd931522eed1ebe87d8d263cb6eab124952f0e6
DeathCrushZzZ <Rustforthefuture@yandex.ru>
2026-06-24 19:54:52 +0300

    feat(ownership): add start vehicle ownership command

--- crates/application/src/ownership/commands.rs
+++ crates/application/src/ownership/commands.rs
@@ -1,1 +1,11 @@
+use domain::vehicle_ownership::state::OwnershipType;
+use domain::{CustomerId, VehicleId, VehicleOwnershipId};
 
+/// Command to start vehicle ownership
+#[derive(Debug, Clone)]
+pub struct StartVehicleOwnershipCommand {
+    pub ownership_id: VehicleOwnershipId,
+    pub vehicle_id: VehicleId,
+    pub owner_customer_id: CustomerId,
+    pub ownership_type: OwnershipType,
+}
