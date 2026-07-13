3f03ce0fda52bec06cc9ca51df928a3103b55e4a
DeathCrushZzZ <Rustforthefuture@yandex.ru>
2026-07-13 15:06:36 +0300

    feat(application): add get customer handler to fetch customer by id

--- crates/application/src/customer/handlers.rs
+++ crates/application/src/customer/handlers.rs
@@ -2,6 +2,7 @@ use crate::customer::commands::CreateCustomerCommand;
 use crate::customer::ports::CustomerRepository;
 use crate::error::ApplicationError;
 use chrono::Utc;
+use domain::CustomerId;
 use domain::customer::aggregate::Customer;
 use std::sync::Arc;
 
@@ -22,5 +23,20 @@ impl CreateCustomerHandler {
         self.repository.save(&customer)?;
 
         Ok(())
+    }
+}
+
+/// Handler for searching for a customer by ID
+pub struct GetCustomerHandler {
+    repository: Arc<dyn CustomerRepository>,
+}
+
+impl GetCustomerHandler {
+    pub fn new(repository: Arc<dyn CustomerRepository>) -> Self {
+        Self { repository }
+    }
+
+    pub fn handle(&self, customer_id: CustomerId) -> Result<Option<Customer>, ApplicationError> {
+        Ok(self.repository.find_by_id(customer_id)?)
     }
 }
