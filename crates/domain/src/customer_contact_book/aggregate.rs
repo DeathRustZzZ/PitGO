e72a328df96f64fa13666cb6e937904a3ff97931
DeathCrushZzZ <Rustforthefuture@yandex.ru>
2026-07-21 16:10:36 +0300

    refactor(customer_contact_book): add aggregate_version to contact book

--- crates/domain/src/customer_contact_book/aggregate.rs
+++ crates/domain/src/customer_contact_book/aggregate.rs
@@ -1,7 +1,13 @@
+use shared::aggregate::AggregateVersion;
+
 use crate::{customer_contact_book::value_objects::phone_contact::PhoneContact, CustomerId};
 
 #[derive(Debug)]
 pub struct CustomerContactBook {
     customer_id: CustomerId,
     phone_contact: PhoneContact,
+    // email_contact: EmailContact,
+    // telegram_contact: TelegramContact,
+    // primary_contact: PrimaryContact,
+    aggregate_version: AggregateVersion,
 }
